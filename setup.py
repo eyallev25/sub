1d8563e611aa423f6cb5f41421b400d57a16ffe1
eyallev <eyallev@eyallev-Mac-1.local>
2022-04-10 19:58:48 +0300

    first commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+setup(name='sub',
+      version='1.0',
+      description='Test',
+      author='Me',
+      author_email='me@example.com',
+      license='MIT',
+      packages=find_packages(),
+      zip_safe=False)
